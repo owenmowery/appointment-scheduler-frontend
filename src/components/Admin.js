9dd265d7ee503b34f5e226dbdcddcc5dec331f29
Owen Mowery <owenmowery99@gmail.com>
2021-07-16 17:07:28 -0400

    worked on login form

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import SignIn from './signInForm'
 
 export default class Admin extends Component {
+
+
     render() {
         return (
             <section className="row">
                 <div className="col span-1-of-2 admin-col">
                     <h1>Admin Page</h1>
+                    <button onClick={() => <SignIn />}>Login</button>
                 </div>
                 <div className="col span-1-of-2 admin-col">
                     <SignIn />
--- src/components/signInForm.js
+++ src/components/signInForm.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import { tsConstructorType } from '@babel/types';
+import React, { Component } from 'react';
+//import axios from 'axios';
 
-const SignIn = () => {
-    return (
-        <h3>Admin Login</h3>
-        <form onSubmit={this.handleSubmit}>
-            <label>Email:</label>
-            <input type="email" value={} />
-        </form>
-    )
+export default class SignIn extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit(event) {
+
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <h3>Admin Login</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Email:</label>
+                    <input type="email" value={this.state.email} />
+                    <br/>
+                    <label>Password:</label>
+                    <input type="password" value={this.state.password} />
+                    <br/>
+                    <input type="submit" value="Submit" />
+                </form>
+            </>
+        )
+    }
 }
-
-export default SignIn
