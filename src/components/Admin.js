7dab7407799b9600441a15335dc94c5b08208224
Owen Mowery <owenmowery99@gmail.com>
2021-07-21 16:27:37 -0400

    edited and styled appointment cols

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -3,14 +3,13 @@ import SignIn from './signInForm'
 
 export default class Admin extends Component {
 
-
     render() {
         return (
             <section className="row">
                 <div className="col span-1-of-2 admin-cols">
                     <h3>Welcome, Admin</h3>
                     <p>Please login to view scheduled appointments</p>
-                    <button className="button" onClick={() => <SignIn />}>Login</button>
+                    <button className="button" >Login</button>
                 </div>
                 <div className="col span-1-of-2 admin-cols">
                     <SignIn />
--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 export default class Appointment extends Component {
     render() {
         return (
-            <h1>Appointment</h1>
+            <div>
+                <section className="row appointment-container">
+                    <div className="appt-cols col span-1-of-2">
+                        <h3>Appointment</h3>
+                    </div>
+                    <div className="appt-cols col span-1-of-2">
+                        <h3>Recently scheduled appointments</h3>
+                    </div>
+                </section>
+            </div>
         )
     }
 }
