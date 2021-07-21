a995c16665e8df18f19051e72ab39c4aabb9acf9
Owen Mowery <owenmowery99@gmail.com>
2021-07-21 15:18:16 -0400

    edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
     render() {
         return (
-            <section className="row footer-row">
-                <p>Footer</p>
-            </section>
+            <div className="footer">
+                <h3>Footer</h3>
+            </div>
         )
     }
 }
