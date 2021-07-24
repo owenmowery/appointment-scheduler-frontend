c13432a015b71971f72a7848ce8cfaec79aa05c9
Owen Mowery <owenmowery99@gmail.com>
2021-07-23 20:00:29 -0400

    routed new appt to actions

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import AddAppointment from './AddAppointment';
 
 export default class Appointment extends Component {
+
+
+    addAppt = (appointment) => {
+        appointment.id = Math.random();
+
+    }
+
     render() {
         return (
             <div>
                 <section className="row appointment-container">
                     <div className="appt-cols col span-1-of-2">
                         <h3>Schedule an Appointment</h3>
-                        <AddAppointment />
+                        <AddAppointment addAppt={this.addAppt} />
                     </div>
                     <div className="appt-cols col span-1-of-2">
                         <h3>Recently scheduled appointments</h3>
